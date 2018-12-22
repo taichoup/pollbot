fa40a70f0f64aa10fb92897a531b6ce183a4eeee
Manuel Dalle <mdalle@dashlane.com>
2018-12-22 12:40:46 +0100

    tutorial bot

---
+++ index.js
@@ -0,0 +1,48 @@
+var Slackbot = require("slackbots");
+var channel = "general";
+
+var bot = new Slackbot({
+    token: "xoxb-509882773761-510486476098-3jbQcUrfdTLIGR47JxebVEKW",
+    name: "testpollbot"
+});
+
+bot.on("start", function() {
+    bot.postMessageToChannel(channel, "Hello world!");
+    console.log("Hello, world!");
+});
+
+// a "message" according to the Slack API isn't necessarily a message of text sent. A "message" is more like a generic event that could be basically anything.
+bot.on("message", function(data) {
+    if (data.type !== "message") { // so we need to check that the message is of type message
+        return;
+    }
+
+    handleMessage(data.text);
+});
+
+function handleMessage(message) {
+    switch(message) {
+        case "hi":
+        case "hello":
+            sendGreeting();
+            break;
+        default:
+            return;
+    }
+}
+
+function sendGreeting() {
+    var greeting = getGreeting();
+    bot.postMessageToChannel(channel, greeting);
+}
+
+function getGreeting() {
+    var greetings = [
+        "hello!",
+        "hi there!",
+        "cheerio!",
+        "how do you do!",
+        "coucou!"
+    ];
+    return greetings[Math.floor(Math.random() * greetings.length)];
+}
